d4b55dec28ac3fe6062ba5cb061df339f134a19e
Biniyam Agegnehu <biniyamagegnehu2@gmail.com>
2026-03-12 22:47:01 +0300

    create api utility

---
+++ auth-system/client/src/utils/api.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: 'http://localhost:5000/api'
+});
+
+// Request interceptor to add token to headers
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response interceptor to handle errors
+API.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      // Unauthorized - clear token and redirect to login
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default API;
