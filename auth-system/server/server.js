fc1f4f3d522569981e18229c3cfb47eb7282bf57
Biniyam Agegnehu <biniyamagegnehu2@gmail.com>
2026-03-12 21:29:25 +0300

    Update server.js to connect to MongoDB

--- auth-system/server/server.js
+++ auth-system/server/server.js
@@ -9,13 +9,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Basic route for testing
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// Basic route
 app.get('/', (req, res) => {
   res.json({ message: 'Auth system API is running' });
 });
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
